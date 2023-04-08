f3ba9fc64c00c2abe8a1e7316206f987a5effe67
Olha <99271539+Olha-Khr@users.noreply.github.com>
2023-04-08 03:19:35 +0300

    fix

---
+++ src/components/Section/Section.styled.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Indent = styled.section`
+padding-left: 15px;
+padding-right: 15px;
+margin: 10px;
+color: rgb(0, 0, 225);
+border:dotted rgb(30, 80, 300, 0.5);
+border-width: 8px 4px;
+border-radius: 10px 50px;`
+
